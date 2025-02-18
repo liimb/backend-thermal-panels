7a87daba6158ce781b1e02caee7c7ba002b57919
Артемий <freedomskyleva@gmail.com>
2025-02-18 22:52:05 +0600

    Initial commit

---
+++ src/main/java/com/thermal/thermalback/ThermalBackApplication.java
@@ -0,0 +1,13 @@
+package com.thermal.thermalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermalBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThermalBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thermal/thermalback/ThermalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thermal.thermalback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThermalBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
