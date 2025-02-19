68fe8b5b21c31146e1f0b792c16ba1351df9ced9
Артемий <freedomskyleva@gmail.com>
2025-02-19 11:53:42 +0600

    first settings

---
+++ src/main/java/com/thermal/thermalback/config/WebSecurityConfig.java
@@ -0,0 +1,77 @@
+package com.thermal.thermalback.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(getOpenedResources()).hasAnyRole("ADMIN")
+                        .requestMatchers(
+                                "/api/users/**",
+                                "/api/roles/**",
+                                "/home/users/**").hasRole("ADMIN")
+                        .requestMatchers(
+                                "/api/sizes/**",
+                                "/api/brands/**",
+                                "/api/products/**",
+                                "/home/sizes/**",
+                                "/home/brands/**",
+                                "/home/products/**").hasAnyRole("ADMIN", "MANAGER")
+                        .requestMatchers(
+                                "/api/buyers/**",
+                                "/api/orders/**",
+                                "/home",
+                                "/home/buyers/**",
+                                "/home/orders/**",
+                                "/api/sellers",
+                                "/api/productsItems/**",
+                                "/home/productsItems/**",
+                                "/js/**").hasAnyRole("ADMIN", "MANAGER", "CASHIER")
+                        .requestMatchers(
+                                "/").hasAnyRole("ADMIN", "MANAGER", "CASHIER", "USER"))
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
+    private String[] getOpenedResources() {
+        return new String[]{
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/swagger-resources/**",
+                "/swagger-resources/**",
+                "/v3/api-docs/**",
+                "/v3/api-docs/**",
+                "/register/**"
+        };
+    }
+}
